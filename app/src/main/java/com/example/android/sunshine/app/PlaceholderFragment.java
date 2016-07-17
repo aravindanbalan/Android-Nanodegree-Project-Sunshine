a15a0c3983b2b26535dcdb46d83609caed5272a8
Aravindan Balan <arbalan@paypal.com>
2016-07-17 11:00:12 -0700

    connect adapter with listview

--- app/src/main/java/com/example/android/sunshine/app/PlaceholderFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/PlaceholderFragment.java
@@ -9,6 +9,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,8 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class PlaceholderFragment extends Fragment {
+
+    ArrayAdapter<String> mForecastAdapter;
 
     public PlaceholderFragment() {
     }
@@ -34,6 +38,10 @@ public class PlaceholderFragment extends Fragment {
         forecastList.add("Fri - Foggy - 70 / 46");
         forecastList.add("Sat - Sunny - 76 / 68");
 
+        mForecastAdapter = new ArrayAdapter<>(getActivity(), R.layout.view_list_item_forecast, R.id.list_item_forecast_textview, forecastList);
+
+        ListView forecastListView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        forecastListView.setAdapter(mForecastAdapter);
         return rootView;
     }
 }
