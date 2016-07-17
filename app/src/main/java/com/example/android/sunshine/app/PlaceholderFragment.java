e0c6a769db0ee01ec6d31bb9dd001b4f6fc00b0b
Aravindan Balan <arbalan@paypal.com>
2016-07-16 22:31:13 -0700

    Added new list view with dummy data

--- app/src/main/java/com/example/android/sunshine/app/PlaceholderFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/PlaceholderFragment.java
@@ -10,6 +10,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -22,6 +25,15 @@ public class PlaceholderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        List<String> forecastList = new ArrayList<>();
+        forecastList.add("Today - Sunny - 88 / 63");
+        forecastList.add("Tomorrow - Foggy - 70 / 46");
+        forecastList.add("Weds - Cloudy - 72 / 63");
+        forecastList.add("Thurs - Rainy - 64 / 51");
+        forecastList.add("Fri - Foggy - 70 / 46");
+        forecastList.add("Sat - Sunny - 76 / 68");
+
         return rootView;
     }
 }
