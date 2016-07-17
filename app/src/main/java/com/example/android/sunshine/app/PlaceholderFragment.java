408f75c407aa3dd85d5805c0001ee9ca19a2073e
Aravindan Balan <arbalan@paypal.com>
2016-07-16 21:53:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/app/PlaceholderFragment.java
@@ -0,0 +1,27 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by arbalan on 7/16/16.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    public PlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
